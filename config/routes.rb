e2805ef7b4dbca96a793226e512ea5f9990c343f
Al Francis <fr4nc1stein@gmail.com>
2019-07-29 13:47:48 +0800

    Add icons route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'home/index', as: "index"
+  get 'home/icon', as: "icon"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :articles
  
-  root 'welcome#index'
+  root 'home#index'
 end
